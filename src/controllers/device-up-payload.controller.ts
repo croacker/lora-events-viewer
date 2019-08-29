4d9f7497ef2866a5ded955ca90492e453414177c
croacker <croacker@mail.ru>
2019-08-29 17:51:14 +0800

    work

--- src/controllers/device-up-payload.controller.ts
+++ src/controllers/device-up-payload.controller.ts
@@ -1,11 +1,11 @@
 // Uncomment these imports to begin using these cool features!
-import {RestBindings, get, ResponseObject, param} from '@loopback/rest';
-import {inject} from '@loopback/context';
-import {Request} from 'express';
-import {DeviceUpPayload} from '../models';
+import { RestBindings, get, ResponseObject, param } from '@loopback/rest';
+import { inject } from '@loopback/context';
+import { Request } from 'express';
+import { DeviceUpPayload } from '../models';
 // import {inject} from '@loopback/context';
 
-const PING_RESPONSE: ResponseObject = {
+const DEVICE_UP_RESPONSE: ResponseObject = {
   description: 'Ping Response',
   content: {
     'application/json': {
@@ -33,24 +33,20 @@ export class DeviceUpPayloadController {
 
   @get('/device-up-payload/from-hex/{hexStr}', {
     responses: {
-      '200': PING_RESPONSE,
+      '200': DEVICE_UP_RESPONSE,
     },
   })
   async fromHex(
     @param.path.string('hexStr') hexStr: string
-    ): Promise<object> {
-
+  ): Promise<object> {
     hexStr = hexStr || "0c27021e1a57025bb613034226690364a1ef635df7826da64fa24e988024bc5b71e0893e97fcd60c20bbb300900b00000000";
     const buf = Buffer.from(hexStr, "hex");
-    console.log(hexStr);
-    console.log(buf);
-
     return this.bufferToModel(buf);
   }
 
   @get('/device-up-payload/from-base64/{base64Str}', {
     responses: {
-      '200': PING_RESPONSE,
+      '200': DEVICE_UP_RESPONSE,
     },
   })
   async fromBase64(
@@ -58,122 +54,86 @@ export class DeviceUpPayloadController {
   ): Promise<object> {
 
     const buf = Buffer.from(base64Str, "base64");
-    console.log(base64Str);
-    console.log(buf);
-
     return this.bufferToModel(buf);
   }
 
 
-  bufferToModel(buf: Buffer){
+  bufferToModel(buf: Buffer) {
+    const deviceUpPayload = new DeviceUpPayload()
+
     let part = buf.slice(0, 1).reverse()
     let hex = this.toHex(part as Buffer)
-    const protocolVersion = parseInt(hex, 16) / 10
-    console.log(`protocolVersion = ${protocolVersion}`);
+    deviceUpPayload.protocolVersion = parseInt(hex, 16) / 10
 
     part = buf.slice(1, 3).reverse()
     hex = this.toHex(part as Buffer)
-    const commandId = hex
-    console.log(`commandId = ${commandId}`);
+    deviceUpPayload.commandId = hex
 
     part = buf.slice(3, 7).reverse()
     hex = this.toHex(part as Buffer)
-    const longitude = parseInt(hex, 16) * 0.000001
-    console.log(`longitude = ${longitude}`);
+    deviceUpPayload.longitude = parseInt(hex, 16) * 0.000001
+
 
     part = buf.slice(7, 11).reverse()
     hex = this.toHex(part as Buffer)
-    const latitude = parseInt(hex, 16) * 0.000001
-    console.log(`latitude = ${latitude}`);
+    deviceUpPayload.latitude = parseInt(hex, 16) * 0.000001
 
     part = buf.slice(11, 12).reverse()
     hex = this.toHex(part as Buffer)
-    const gpsFixStatus = parseInt(hex, 16) / 32
-    console.log(`gpsFixStatus = ${gpsFixStatus}`);
+    deviceUpPayload.gpsFixStatus = parseInt(hex, 16) / 32
 
     part = buf.slice(12, 13).reverse()
     hex = this.toHex(part as Buffer)
-    const reserve = hex
-    console.log(`reserve = ${reserve}`);
+    deviceUpPayload.reserve = hex
 
     part = buf.slice(13, 15).reverse()
     hex = this.toHex(part as Buffer)
-    const calorie = parseInt(hex, 16)
-    console.log(`calorie = ${calorie}`);
+    deviceUpPayload.calorie = parseInt(hex, 16)
 
     part = buf.slice(15, 16).reverse()
     hex = this.toHex(part as Buffer)
-    const batteryCapacity = parseInt(hex, 16)
-    console.log(`batteryCapacity = ${batteryCapacity}`);
+    deviceUpPayload.batteryCapacity = parseInt(hex, 16)
 
     part = buf.slice(16, 20).reverse()
     hex = this.toHex(part as Buffer)
-    const dateTime = parseInt(hex, 16)
-    console.log(`dateTime = ${dateTime}`);
+    deviceUpPayload.dateTime = parseInt(hex, 16)*1000
 
     part = buf.slice(20, 40).reverse()
     hex = this.toHex(part as Buffer)
-    const beaconId = hex
-    console.log(`beaconId = ${beaconId}`);
+    deviceUpPayload.beaconId = hex
 
     part = buf.slice(40, 41).reverse()
     hex = this.toHex(part as Buffer)
-    const beaconType = parseInt(hex, 16) / 32
-    console.log(`beaconType = ${beaconType}`);
+    deviceUpPayload.beaconType = parseInt(hex, 16) / 32
 
     part = buf.slice(41, 42).reverse()
     hex = this.toHex(part as Buffer)
-    const rssi = hex
-    console.log(`rssi = ${rssi}`);
+    deviceUpPayload.rssi = hex
 
     part = buf.slice(42, 43).reverse()
     hex = this.toHex(part as Buffer)
-    const txPower = parseInt(hex, 16)
-    console.log(`txPower = ${txPower}`);
+    deviceUpPayload.txPower = parseInt(hex, 16)
 
     part = buf.slice(43, 44).reverse()
     hex = this.toHex(part as Buffer)
-    const heartRate = parseInt(hex, 16)
-    console.log(`heartRate = ${heartRate}`);
+    deviceUpPayload.heartRate = parseInt(hex, 16)
 
     part = buf.slice(44, 46).reverse()
     hex = this.toHex(part as Buffer)
-    const temperature = parseInt(hex, 16) / 100
-    console.log(`temperature = ${temperature}`);
+    deviceUpPayload.temperature = parseInt(hex, 16) / 100
 
     part = buf.slice(46, 48).reverse()
     hex = this.toHex(part as Buffer)
-    const step = parseInt(hex, 16)
-    console.log(`step = ${step}`);
+    deviceUpPayload.step = parseInt(hex, 16)
 
     part = buf.slice(48, 50).reverse()
     hex = this.toHex(part as Buffer)
-    const distance = parseInt(hex, 16)
-    console.log(`distance = ${distance}`);
-
-    const deviceUpPayload = new DeviceUpPayload()
-    deviceUpPayload.protocolVersion = protocolVersion
-    deviceUpPayload.commandId = commandId
-    deviceUpPayload.longitude = longitude
-    deviceUpPayload.latitude = latitude
-    deviceUpPayload.gpsFixStatus = gpsFixStatus
-    deviceUpPayload.reserve = reserve
-    deviceUpPayload.calorie = calorie
-    deviceUpPayload.batteryCapacity = batteryCapacity
-    deviceUpPayload.dateTime = dateTime
-    deviceUpPayload.beaconId = beaconId
-    deviceUpPayload.beaconType = beaconType
-    deviceUpPayload.rssi = rssi
-    deviceUpPayload.txPower = txPower
-    deviceUpPayload.heartRate = heartRate
-    deviceUpPayload.temperature = temperature
-    deviceUpPayload.step = step
-    deviceUpPayload.distance = distance
+    deviceUpPayload.distance = parseInt(hex, 16)
 
     return deviceUpPayload;
   }
 
-  toHex(buf: Buffer):string {
+  toHex(buf: Buffer): string {
     return buf.toString('hex')
   }
 
